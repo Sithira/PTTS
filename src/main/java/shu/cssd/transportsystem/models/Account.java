05e3acee9dfc29a4e108dc762deccdce1a64adfa
Sithira Munasinghe <sithiraac@gmail.com>
2018-07-10 13:30:42 +0530

    Reorganized classes into correct packages

---
+++ src/main/java/shu/cssd/transportsystem/models/Account.java
@@ -0,0 +1,8 @@
+package shu.cssd.transportsystem.models;
+
+import shu.cssd.transportsystem.foundation.BaseModel;
+
+public class Account extends BaseModel
+{
+
+}
--- src/main/java/shu/cssd/transportsystem/models/User.java
+++ src/main/java/shu/cssd/transportsystem/models/User.java
@@ -8,11 +8,36 @@ package shu.cssd.transportsystem.models;
 import shu.cssd.transportsystem.foundation.BaseModel;
 
 /**
- *
  * @author sithira
  */
-public class User extends BaseModel {
+public class User extends BaseModel
+{
 
+	public String name;
+	
+	public String email;
+	
+	public String address;
 
-    
+	public String city;
+	
+	public String postalCode;
+	
+	/**
+	 * Creates a new User Object that can be saved in the collection.
+	 *
+	 * @param name
+	 * @param email
+	 * @param address
+	 * @param city
+	 * @param postalCode
+	 */
+	public User(String name, String email, String address, String city, String postalCode)
+	{
+		this.name = name;
+		this.email = email;
+		this.address = address;
+		this.city = city;
+		this.postalCode = postalCode;
+	}
 }
