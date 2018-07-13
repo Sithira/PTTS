eab480b672b946d78e0ba92e299d37d4cefbebea
Chathu Abeywickrama <chathu.git93@gmail.com>
2018-07-13 14:57:48 +0530

    login page now navigates to Dashboard

--- src/main/java/shu/cssd/transportsystem/LoginController.java
+++ src/main/java/shu/cssd/transportsystem/LoginController.java
@@ -8,50 +8,58 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import shu.cssd.transportsystem.dashboard.DashboardController;
 
 public class LoginController implements Initializable
 {
-	
-	@FXML
-	private Label label;
+
 	@FXML
 	private TextField username;
 	@FXML
 	private PasswordField password;
-	
+
+
 	@FXML
 	AnchorPane rootPane;
 	
 	@FXML
-	private void handleButtonAction(ActionEvent event)
+	private void login_buttonClick(ActionEvent event) throws IOException
 	{
+        if (username.getText().equals("admin") && password.getText().equals("admin")){
+            Parent dashboardParent = FXMLLoader.load(getClass().getResource("/dashboard/Dashboard.fxml"));
+            Scene dashboardScene = new Scene(dashboardParent);
+
+            Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+
+            window.setScene(dashboardScene);
+            window.show();
+        }
+        else{
+            Parent dashboardParent = FXMLLoader.load(getClass().getResource("/login/Scene.fxml"));
+            Scene dashboardScene = new Scene(dashboardParent);
+
+            Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+
+            window.setScene(dashboardScene);
+            window.show();
+        }
+
 
 	}
 	
 	@Override
 	public void initialize(URL url, ResourceBundle rb)
 	{
-		//username =
+
 	}
 	
-	public void login() throws IOException
-	{
-		if (username.getText().equals("admin") && password.getText().equals("admin"))
-		{
-			
-			AnchorPane root = FXMLLoader.load(getClass().getResource("/dashboard/Dashboard.fxml"));
-			
-			rootPane.getChildren().setAll(root);
-			
-		}
-	}
-	
-	public void switchToMainDashboard(ActionEvent event)
-	{
-		//(new DashboardController()).loadDashBoard(event);
-	}
+
 }
