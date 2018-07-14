52971e373b7c5e027d62a4c71974a9111cd0dcde
Sithira Munasinghe <sithiraac@gmail.com>
2018-07-14 19:17:48 +0530

    Cleared JouneryController

--- src/main/java/shu/cssd/transportsystem/controllers/JourneyController.java
+++ src/main/java/shu/cssd/transportsystem/controllers/JourneyController.java
@@ -1,29 +1,6 @@
 package shu.cssd.transportsystem.controllers;
 
-import shu.cssd.transportsystem.models.Journey;
-import shu.cssd.transportsystem.models.collections.SetOfJourney;
-
 public class JourneyController
 {
-    private SetOfJourney setOfJourney = new SetOfJourney();
 
-    private Journey currentJourney;
-
-    /**
-     * Creates a new journey in the system
-     *
-     * @param routeId
-     * @param originId
-     * @param destinationId
-     * @param cost
-     * @return Returns a boolean depending on the status of creation
-     */
-    public boolean createJourney(String userId, String routeId, String originId, String destinationId, Double cost)
-    {
-        Journey journey = new Journey(userId, routeId, originId, destinationId, cost);
-
-        this.currentJourney = journey;
-
-        return setOfJourney.create(journey);
-    }
 }
--- src/main/java/shu/cssd/transportsystem/controllers/ui/MobileAppController.java
+++ src/main/java/shu/cssd/transportsystem/controllers/ui/MobileAppController.java
@@ -1,9 +1,11 @@
 package shu.cssd.transportsystem.controllers.ui;
 
 import shu.cssd.transportsystem.controllers.UserController;
+import shu.cssd.transportsystem.foundation.BaseModel;
 import shu.cssd.transportsystem.models.Route;
 import shu.cssd.transportsystem.models.Stop;
 import shu.cssd.transportsystem.models.User;
+import shu.cssd.transportsystem.models.collections.SetOfRoutes;
 
 import java.util.ArrayList;
 
@@ -12,6 +14,13 @@ public class MobileAppController
 	
 	private User user;
 	
+	/**
+	 * Login the user
+	 *
+	 * @param username username of the user
+	 * @param password password of the user
+	 * @return boolean
+	 */
 	public boolean login(String username, String password)
 	{
 		
@@ -24,6 +33,11 @@ public class MobileAppController
 		return login;
 	}
 	
+	/**
+	 * Logout the user
+	 *
+	 * @return boolean
+	 */
 	public boolean logout()
 	{
 		this.user = null;
@@ -31,9 +45,25 @@ public class MobileAppController
 		return true;
 	}
 	
+	/**
+	 * Get the stops that belongs to a route
+	 *
+	 * @param route {@link Route}
+	 * @return {@link Stop}
+	 */
 	public ArrayList<Stop> getStops(Route route)
 	{
 		return route.getStops();
+	}
+	
+	/**
+	 * Get all the routes in the system
+	 *
+	 * @return {@link BaseModel}
+	 */
+	public ArrayList<BaseModel> getRoutes()
+	{
+		return (new SetOfRoutes()).all();
 	}
 	
 }
