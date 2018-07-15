c4e8e04b2b9bbeca26f33372bc042fff853002fe
Chathu Abeywickrama <chathu.git93@gmail.com>
2018-07-15 18:20:33 +0530

    smartcard and token scenes implemented partially

--- src/main/java/shu/cssd/transportsystem/views/tokenMachine/HomeController.java
+++ src/main/java/shu/cssd/transportsystem/views/tokenMachine/HomeController.java
@@ -21,4 +21,26 @@ public class HomeController {
         window.setScene(ticketsScene);
         window.show();
     }
+
+    @FXML
+    private void tokensButtonClick(MouseEvent event) throws IOException {
+        Parent tokensParent = FXMLLoader.load(getClass().getResource("/tokenMachine/tokens/tokens.fxml"));
+        Scene tokensScene = new Scene(tokensParent);
+
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+
+        window.setScene(tokensScene);
+        window.show();
+    }
+
+    @FXML
+    private void logoutButtonClick(MouseEvent event) throws IOException {
+        Parent loginParent = FXMLLoader.load(getClass().getResource("/tokenMachine/login/login.fxml"));
+        Scene loginScene = new Scene(loginParent);
+
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+
+        window.setScene(loginScene);
+        window.show();
+    }
 }
