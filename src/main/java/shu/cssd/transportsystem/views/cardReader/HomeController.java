29f9cdaabd50d79cafdb7d060ccbda1402e2cb85
Chathu Abeywickrama <chathu.git93@gmail.com>
2018-07-17 11:43:54 +0530

    Card Reader is Done

---
+++ src/main/java/shu/cssd/transportsystem/views/cardReader/HomeController.java
@@ -0,0 +1,26 @@
+package shu.cssd.transportsystem.views.cardReader;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HomeController {
+
+    @FXML
+    private void scanButtonClick(MouseEvent event) throws IOException {
+        Parent empParent = FXMLLoader.load(getClass().getResource("/cardReader/success/success.fxml"));
+        Scene empScene = new Scene(empParent);
+
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+
+        window.setScene(empScene);
+        window.show();
+    }
+
+}
